2ccdd594b0064fc3bc1f683ff97c5b7ad40cc7ca
Mouhamadou-MD <64435997+Mouhamadou-MD@users.noreply.github.com>
2023-03-25 22:55:20 +0100

    Update appli.py

--- toolbox/appli.py
+++ toolbox/appli.py
@@ -41,6 +41,7 @@ def scan_vuln():
 	host = tk.simpledialog.askstring("Hôte à scanner", "Entrez l'adresse IP de l'hôte ou du réseau à scanner:")
 	file_name = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") +"-Vuln_Nmap"+ ".txt"
 	with open(file_name, "w") as f:
+		#mis à jour de la base de donneés + Lancement du scan 
 		cmd= f"sudo nmap --script-updatedb && nmap -Pn -sV --script vuln {host}>{file_name}"
 		subprocess.run(cmd, shell=True)
 	tk.messagebox.showinfo("Scan terminé", f"Le scan de {host} est terminé. Les résultats ont été enregistrés dans le fichier {file_name}.")
